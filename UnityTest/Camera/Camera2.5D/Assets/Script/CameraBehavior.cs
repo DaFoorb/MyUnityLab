f754c00bdfb6f78317a1c3093f7f0c663a07c23d
DaFoorb <o.cornupro@gmail.com>
2016-07-08 01:45:45 +0200

    Mise à jour Camera : Ajout des modes

--- UnityTest/Camera/Camera2.5D/Assets/Script/CameraBehavior.cs
+++ UnityTest/Camera/Camera2.5D/Assets/Script/CameraBehavior.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CameraBehavior : MonoBehaviour {
@@ -7,23 +8,70 @@ public class CameraBehavior : MonoBehaviour {
 	public int posY ;
 	public int posZ ;
 
+	int i_mode ;
+
+	float f_distance ;
+	float f_ratio ;
+
 	public GameObject go_mainCharacter ;
+	public GameObject go_level ;
+	public Text t_text ;
 
 	// Use this for initialization
 	void Start () {
-	
+		f_distance= go_level.transform.localScale.x + 30.0f ;
+
+		f_ratio= f_distance/go_level.transform.localScale.x ;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		// Déplacement en même temps que le personnage
-		transform.position = new Vector3 (go_mainCharacter.GetComponent<Transform> ().position.x - posX,
-			go_mainCharacter.GetComponent<Transform> ().position.y + posY,
-			go_mainCharacter.GetComponent<Transform> ().position.z - posZ) ;
-
-
+		cameraMode ();
+		changeCameraMode ();
 		// Regarde vers le main character
-		transform.LookAt (go_mainCharacter.GetComponent<Transform>().position) ;
+		transform.LookAt (go_mainCharacter.transform.position) ;
+	}
+
+
+	void changeCameraMode()
+	{
+		if (Input.GetKey("1")) 
+		{
+			i_mode= 1;
+		}
+
+		if (Input.GetKey("2")) 
+		{
+			i_mode= 2;
+		}
+	}
+
+	void cameraMode () 
+	{
+		switch (i_mode)
+		{
+		case 1:
+			// Toujours derrière l'objet
+			transform.position = new Vector3 (
+				go_mainCharacter.transform.position.x - posX,
+				go_mainCharacter.transform.position.y + posY,
+				go_mainCharacter.transform.position.z - posZ
+			);
+
+			t_text.text = "Camera mode 1 : Follow behind";
+
+			break ;
+		case 2:
+			// Tourne autour de l'objet en fonction de la taille du niveau
+			transform.position = new Vector3 (
+				(go_mainCharacter.transform.position.x - posX) * f_ratio,
+				go_mainCharacter.transform.position.y + posY,
+				go_mainCharacter.transform.position.z - posZ
+			);
+			t_text.text = "Camera mode 2 : Turn around";
+			break ;
+		case 3:
+			break ;
+		}
 	}
 }
